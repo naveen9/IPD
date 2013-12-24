ee96bac46748f06dce002a1f017e7bc3ba5fe68b
sameer <sam.nyx@live.com>
2013-12-24 14:35:28 +0530

    login title change

--- IPD_git1/index.php
+++ IPD_git1/index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Login</title>
+<title>Webhis Login</title>
 <link rel="stylesheet" type="text/css" href="style_by.css" />
 <link rel="stylesheet" type="text/css" href="login-code.css" />
 <script src="hide_show.js" type="text/javascript"></script>
